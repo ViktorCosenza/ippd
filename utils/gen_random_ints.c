11570642cc5510a0c22d00a39290f1b40af93d0e
Victor Cosenza <vcvdsilva@inf.ufpel.edu.br>
2019-09-07 20:13:24 -0300

    fix assert olhando uma posicao a mais e assinatura uint -> int

--- utils/gen_random_ints.c
+++ utils/gen_random_ints.c
@@ -9,8 +9,8 @@ int *gen_random_ints (unsigned int length, unsigned int max) {
     return arr;
 }
 
-int assert_asc_order(int arr[], unsigned int length) {
-    for (int i=0; i < length; ++i)
+int assert_asc_order(int arr[], int length) {
+    for (int i=0; i < length - 1; ++i)
         if (arr[i] > arr[i+1])
             return 0;
     return 1;
